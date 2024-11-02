c61beaa9388f092087138f0d25caf2e7577c6e19
unknown <hksharma85028@gmail.com>
2024-11-02 17:24:25 +0530

    change again

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import SearchOrder from "../features/order/SearchOrder"
 import Username from "../features/user/Username"
 function Header() {
   return (
     <header className="flex px-4 py-3 bg-orange-400 justify-between items-center border-b border-stone-200
     uppercase sm:px-6">
-        <Link to='/pizzashop' className="text-zinc-900 tracking-widest font-semibold">
+        <NavLink to='/pizzashop/' className="text-zinc-900 tracking-widest font-semibold">
         Pizza Shoap
-        </Link>
+        </NavLink>
         <SearchOrder />
         <Username/>
     </header>
